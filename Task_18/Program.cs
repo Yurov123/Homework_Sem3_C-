c76d4080117c81e23135653c8daa59ed50573c4b
Konstantin <constantin8271@gmail.com>
2022-02-08 21:56:16 +0300

    task_18

---
+++ Task_18/Program.cs
@@ -0,0 +1,39 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+
+bool x1 = false;
+bool y1 = false;
+
+if( !(x1|| y1) == (!x1 && !y1) )
+    Console.WriteLine("false" );
+else
+{
+     Console.WriteLine();
+}
+bool x2 = false;
+bool y2 = true;
+
+if( !(x2|| y2) == (!x2 && !y2) )
+    Console.WriteLine("false" );
+else
+{
+     Console.WriteLine();
+}
+bool x3 = true;
+bool y3 = false;
+
+if( !(x3|| y3) == (!x3 && !y3) )
+    Console.WriteLine("false" );
+else
+{
+     Console.WriteLine();
+}
+bool x4 = true;
+bool y4 = true;
+if ( !(x4|| y4) == (!x4 && !y4) )
+    Console.WriteLine(value: "true" );
+else
+{
+     Console.WriteLine();
+}
+
