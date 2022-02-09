f29404f09336e9e7920a642fa772a746a5bc5bc9
Konstantin <constantin8271@gmail.com>
2022-02-09 20:19:54 +0300

    task_18 Option 2

--- Task_18/Program.cs
+++ Task_18/Program.cs
@@ -8,7 +8,7 @@ if( !(x1|| y1) == (!x1 && !y1) )
     Console.WriteLine("false" );
 else
 {
-     Console.WriteLine();
+     Console.WriteLine("true");
 }
 bool x2 = false;
 bool y2 = true;
@@ -17,7 +17,7 @@ if( !(x2|| y2) == (!x2 && !y2) )
     Console.WriteLine("false" );
 else
 {
-     Console.WriteLine();
+     Console.WriteLine("true");
 }
 bool x3 = true;
 bool y3 = false;
@@ -26,14 +26,69 @@ if( !(x3|| y3) == (!x3 && !y3) )
     Console.WriteLine("false" );
 else
 {
-     Console.WriteLine();
+     Console.WriteLine("true");
 }
 bool x4 = true;
 bool y4 = true;
 if ( !(x4|| y4) == (!x4 && !y4) )
-    Console.WriteLine(value: "true" );
+    Console.WriteLine( "true" );
 else
 {
-     Console.WriteLine();
+     Console.WriteLine("false");
 }
+
+
+//другое решение
+
+bool X1 = false;
+bool Y1 = false;
+
+bool X2 = false;
+bool Y2 = true;
+
+bool X3 = true;
+bool Y3 = false;
+
+bool X4 = true;
+bool Y4 = true;
+
+if ( (!(X1 || Y1) == !X1 && !Y1) && (!(X2 || Y2) == !X2 && !Y2) && (!(X3 || Y3) == !X3 && !Y3) && (!(X4 || Y4) == !X4 && !Y4))
+{
+    Console.WriteLine("true");
+}
+else
+{
+    Console.WriteLine("false");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
