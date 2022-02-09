0ba503e27d612b35c1b41e4da717a9ea8434a84f
Konstantin <constantin8271@gmail.com>
2022-02-09 20:41:16 +0300

    task_20

---
+++ Task_20/Program.cs
@@ -0,0 +1,47 @@
+﻿//  Задать номер четверти, показать диапазоны для возможных координат
+// могу ошибаться в решении,но другого варианта не вижу,точнее не понимаю как можно задать четверть
+
+Console.WriteLine("Enter x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+ 
+Console.WriteLine("Enter y: ");
+double y = Convert.ToDouble(Console.ReadLine());
+ 
+if ((x > 0) && (y > 0))
+Console.WriteLine("1 quarter");
+else
+{
+    if ((x < 0) && (y > 0))
+        Console.WriteLine("2 quarter");
+    else
+    {
+    if ((x < 0) && (y < 0))
+        Console.WriteLine("3 quarter");
+    else
+    {
+    if ((x > 0) && (y < 0))
+        Console.WriteLine("4 quarter");
+    else
+        Console.WriteLine("The point lies on the axis");
+    }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
