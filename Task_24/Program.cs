9bb91be57c1ca9557ae49574e39d9e2e7b6af391
Konstantin <constantin8271@gmail.com>
2022-02-10 19:53:34 +0300

    task_24

---
+++ Task_24/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти кубы чисел от 1 до N
+
+
+            Console.WriteLine("Enter number a");
+            double a = Convert.ToDouble(Console.ReadLine()); // от единицы скучно,решил пусть будет от любого числа
+            Console.WriteLine("Enter number b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            while (a <= b)
+            {
+                Console.WriteLine(a);
+                Console.WriteLine((a * a * a));
+                a++;
+            }
+        
+
